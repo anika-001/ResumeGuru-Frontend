1b9a903f24cd505fd27c9e2af0ac7a52275a9a96
anika-001 <dream.sweeter.30.09@gmail.com>
2021-10-17 02:38:26 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AngularFireModule } from '@angular/fire/compat';
 import {AngularFireAuthModule} from '@angular/fire/compat/auth';
 import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
