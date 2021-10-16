3530aec2449f61c158336137bdc3ad3c63de3e6b
Vaishnavi <vaishnavisdesai@gmail.com>
2021-10-16 19:28:27 +0530

    inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'mhacks';
+  apiEndPoint = 'http://localhost:5000/pdf';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+      let formData: FormData = new FormData();
+      formData.append('uploadFile', file, file.name);
+      let headers = new HttpHeaders();
+      /** In Angular 5, including the header Content-Type can invalidate your request */
+      headers.append('Content-Type', 'multipart/form-data');
+      headers.append('Accept', 'application/json');
+      // let options = new RequestOptions({ headers: headers });
+      this.httpClient.post(this.apiEndPoint, formData, {headers: headers})
+        .subscribe(
+          data => console.log('success'),
+          error => console.log(error)
+        )
+    }
+  }
+}
