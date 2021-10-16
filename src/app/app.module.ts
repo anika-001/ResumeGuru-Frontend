842e48d161eedcee196d2d7ebbf7553d4971900d
Vaishnavi <vaishnavisdesai@gmail.com>
2021-10-16 20:11:13 +0530

    home page init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClient,
     HttpClientModule
   ],
   providers: [],
