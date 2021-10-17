467fb205bd5dafb5d2a8893bfbdb60a6f46044cf
Vaishnavi <vaishnavisdesai@gmail.com>
2021-10-17 17:18:42 +0530

    ...

--- src/app/resume-upload/resume-upload.component.ts
+++ src/app/resume-upload/resume-upload.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-resume-upload',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeUploadComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private db: AngularFirestore, private as: AuthService) {}
 
+  user: any;
+
+  projectUploadForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    githubLink: new FormControl(''),
+    projectLink: new FormControl(''),
+    surveyLink: new FormControl(''),
+    description: new FormControl('', [Validators.required])
+  });
+  
   ngOnInit(): void {
+    this.as.getUserState().subscribe(user => {
+      if(user == null) {this.router.navigate(['/signin'])}
+      else{
+        this.user = user;
+        this.as.getprofile(user.uid).subscribe((res:any) => {
+          this.user["name"] = res.payload.data().name;
+        })
+        
+      }
+    })
+    
+  }
+
+  save() {
+    let puf = this.projectUploadForm.value;
+    puf["uid"] = this.user.uid;
+    this.projectUploadForm.get("name").setValue("");
+    this.projectUploadForm.get("githubLink").setValue("");
+    this.projectUploadForm.get("projectLink").setValue("");
+    this.projectUploadForm.get("surveyLink").setValue("");
+    this.projectUploadForm.get("description").setValue("");
+    this.db.collection("Projects").add(puf).then(res => {console.log("Success")}).catch(err => {console.log(err)});
   }
 
 }
