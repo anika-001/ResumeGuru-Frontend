b3f52287c003ea849d24316ecef0d674cc59cb55
Vaishnavi <vaishnavisdesai@gmail.com>
2021-10-17 19:37:58 +0530

    ...

--- src/app/review-project/review-project.component.ts
+++ src/app/review-project/review-project.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-review-project',
@@ -8,11 +11,48 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ReviewProjectComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
-
   id: any;
+  project: any;
+
   ngOnInit(): void {
     this.id = this.route.snapshot.queryParams['id']
+    this.getproject();
+    this.as.getUserState().subscribe(user => {
+      if(user == null) {this.router.navigate(['/signin'])}
+      else{
+        this.user = user;
+        this.as.getprofile(user.uid).subscribe((res:any) => {
+          this.user["name"] = res.payload.data().name;
+        })
+        
+      }
+    })
   }
 
+ 
+
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private router: Router, private db: AngularFirestore, private as: AuthService) {}
+
+  user: any;
+
+  getproject(){
+    this.db.collection("Projects").doc(this.id).snapshotChanges().subscribe(res => {
+      this.project = res;
+    })
+  }
+
+  projectUploadForm = new FormGroup({
+    comments: new FormControl('', [Validators.required]),
+    // githubLink: new FormControl(''),
+    // projectLink: new FormControl(''),
+    // surveyLink: new FormControl(''),
+    // description: new FormControl('', [Validators.required])
+  });
+
+
+  save() {
+    let puf = this.projectUploadForm.value;
+    puf["uid"] = this.user.uid;
+    this.db.collection("Projects").doc(this.id).collection("Comments").add(puf).then(res => {console.log("Success")}).catch(err => {console.log(err)});
+  }
 }
--- src/app/review-resume/review-resume.component.ts
+++ src/app/review-resume/review-resume.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-review-resume',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewResumeComponent implements OnInit {
 
-  constructor() { }
+  id: any;
+  project: any;
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.queryParams['id']
+    this.getproject();
+    this.as.getUserState().subscribe(user => {
+      if(user == null) {this.router.navigate(['/signin'])}
+      else{
+        this.user = user;
+        this.as.getprofile(user.uid).subscribe((res:any) => {
+          this.user["name"] = res.payload.data().name;
+        })
+        
+      }
+    })
+  }
+
+ 
+
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private router: Router, private db: AngularFirestore, private as: AuthService) {}
+
+  user: any;
+
+  getproject(){
+    this.db.collection("Resumes").doc(this.id).snapshotChanges().subscribe(res => {
+      this.project = res;
+    })
+  }
+
+  projectUploadForm = new FormGroup({
+    comments: new FormControl('', [Validators.required]),
+    // githubLink: new FormControl(''),
+    // projectLink: new FormControl(''),
+    // surveyLink: new FormControl(''),
+    // description: new FormControl('', [Validators.required])
+  });
+
+
+  save() {
+    let puf = this.projectUploadForm.value;
+    puf["uid"] = this.user.uid;
+    this.db.collection("Projects").doc(this.id).collection("Comments").add(puf).then(res => {console.log("Success")}).catch(err => {console.log(err)});
   }
 
 }
